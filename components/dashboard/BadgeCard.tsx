28f06d383a94389c5652da72a7924ab83fa3feab
AhmedHarb <ahmedharb960@gmail.com>
2024-08-07 04:17:10 +0300

    fix: resolve BadgeCard types error

--- components/dashboard/BadgeCard.tsx
+++ components/dashboard/BadgeCard.tsx
@@ -1,11 +1,23 @@
 import Image from "next/image";
 import React from "react";
+interface IPropsTypes {
+  title: string;
+  total: number;
+  icon: string;
+  id?: number;
+}
 
-const BadgeCard = ({ title, total, icon }) => {
+const BadgeCard = ({ title, total, icon }: IPropsTypes) => {
   return (
     <article className="bg-[#1E00B9] rounded-2xl lg:p-2 flex gap-4 items-center text-white pl-6 cursor-pointer">
       <div className="bg-white flex justify-center items-center p-2 w-[56px] h-[56px] rounded-[50%]">
-        <Image className="lg:w-5 lg:h-5" src={icon} alt={title} width={30} height={30} />
+        <Image
+          className="lg:w-5 lg:h-5"
+          src={icon}
+          alt={title}
+          width={30}
+          height={30}
+        />
       </div>
       <div className="flex flex-col">
         <p className="font-bold text-xl max-w-[70%] leading-7">{title}</p>
